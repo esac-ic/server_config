146d703e384daef050c4e4461c2f260203ea2180
Wouter (W.L.M.) Ligtenberg <wouter.ligtenberg@ing.com>
2020-02-26 23:08:01 +0200

    fixed bug in update script

--- backup-container/scripts/daily/backup.sh
+++ backup-container/scripts/daily/backup.sh
@@ -11,5 +11,5 @@ mysqldump -h $DB_HOST -u $DB_USERNAME -P $DB_PORT -p$DB_PASSWORD --databases $DB
 tar -zcvf "storage_"$dt".gz" /storage
 
 #copying files to the backup folder
-cp "/etc/periodic/dailystorage_"$dt".gz" /backups
-cp /etc/periodic/daily$DB_DATABASE"_"${dt}.sql /backups
+cp "/etc/periodic/daily/storage_"$dt".gz" /backups
+cp /etc/periodic/daily/$DB_DATABASE"_"${dt}.sql /backups
