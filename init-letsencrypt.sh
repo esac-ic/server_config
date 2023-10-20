e027c2493988c44b44b714e5368eb8c50dc975f6
Wouter Bles <whbles@gmail.com>
2023-10-20 14:54:41 +0200

    Improve docker files

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-
+# Check if docker-compose is installed
 if ! [ -x "$(command -v docker-compose)" ]; then
   echo 'Error: docker-compose is not installed.' >&2
   exit 1
 fi
 
+# Configuration
 domains=(esac.nl)
 rsa_key_size=4096
 data_path="./certs"
 email="certs@onzichtbsaar.net" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
+# Prepare configuration for Nginx
 cp nginx-certbot/data/nginx/app.conf_backup nginx-certbot/data/nginx/app.conf
-sed -i "s/__domain__/$domains/g" nginx-certbot/data/nginx/app.conf
+sed -i "s/__domain__/${domains[0]}/g" nginx-certbot/data/nginx/app.conf
 
+# Prompt user for confirmation if data path exists
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for ${domains[0]}. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
 fi
 
-
+# Download SSL parameters if they don't exist
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
@@ -31,43 +34,42 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+# Create a dummy certificate
+echo "### Creating dummy certificate for ${domains[0]} ..."
+path="/etc/letsencrypt/live/${domains[0]}"
+mkdir -p "$data_path/conf/live/${domains[0]}"
 docker-compose -f nginx-certbot/docker-compose.yml run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:1024 -days 1\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
 echo
 
-
+# Start nginx
 echo "### Starting nginx ..."
 docker-compose -f nginx-certbot/docker-compose.yml up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+# Remove the dummy certificate
+echo "### Deleting dummy certificate for ${domains[0]} ..."
 docker-compose -f nginx-certbot/docker-compose.yml run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/live/${domains[0]} && \
+  rm -Rf /etc/letsencrypt/archive/${domains[0]} && \
+  rm -Rf /etc/letsencrypt/renewal/${domains[0]}.conf" certbot
 echo
 
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
+# Request Let's Encrypt certificate
+echo "### Requesting Let's Encrypt certificate for ${domains[0]} ..."
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
 done
 
-# Select appropriate email arg
 case "$email" in
   "") email_arg="--register-unsafely-without-email" ;;
   *) email_arg="--email $email" ;;
 esac
 
-# Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose -f nginx-certbot/docker-compose.yml run --rm --entrypoint "\
@@ -80,6 +82,7 @@ docker-compose -f nginx-certbot/docker-compose.yml run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
+# Reload nginx
 echo "### Reloading nginx ..."
 docker-compose -f nginx-certbot/docker-compose.yml exec nginx nginx -s reload
 docker-compose -f nginx-certbot/docker-compose.yml down
--- init.sh
+++ init.sh
@@ -1,7 +1,31 @@
-#run this script first time after pulling to set the right userrights
-chown -R ic:ic .
-chown -R www-data:www-data storage/
+#!/bin/bash
+
+# This script sets the appropriate user permissions for various files and directories.
+
+# Define the users and groups
+USER_IC="ic"
+GROUP_IC="ic"
+USER_WWW_DATA="www-data"
+GROUP_WWW_DATA="www-data"
+
+# Verify that the script is running as root, otherwise it may not have permission to change ownership
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Change ownership of all files and directories in the current directory to 'ic' user and group
+chown -R "${USER_IC}:${GROUP_IC}" .
+
+# Change ownership of 'storage/' directory to 'www-data' user and group for web server access
+chown -R "${USER_WWW_DATA}:${GROUP_WWW_DATA}" storage/
+
+# Ensure the versions file exists and is owned by 'ic' user
 touch storage/app/public/versions.txt
-chown ic:ic storage/app/public/versions.txt
+chown "${USER_IC}:${GROUP_IC}" storage/app/public/versions.txt
+
+# Make scripts executable
 chmod u+x start.sh
 chmod u+x update.sh
+
+echo "User permissions have been set."
